17c71f33394ae4d10d92f33b47b92faf448b94c7
Eric Intihar <e.intihar@hotmail.com>
2017-04-03 15:03:53 -0400

    IngredientSqlDAL added

---
+++ Capstone.Web/DAL/IngredientSqlDAL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Capstone.Web.Models;
+using System.Data.SqlClient;
+
+namespace Capstone.Web.DAL
+{
+    public class IngredientSqlDAL
+    {
+        public List<Ingredient> GetAllIngredients()
+        {
+            List<Ingredient> listOfIngredients = new List<Ingredient>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(@"Data Source=.\SQLEXPRESS;Initial Catalog=ingredients;User ID=te_student;Password=sqlserver1"))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("select * from ingredient", conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Ingredient i = new Ingredient()
+                        {
+                            Name = Convert.ToString(reader["ingredient_name"]),
+                            Id = int.Parse(Convert.ToString(reader["ingredient_id"])),
+                            quantity = Convert.ToString(reader["quantity"])
+                            };
+                        listOfIngredients.Add(i);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return listOfIngredients;
+        }
+    }
+}
