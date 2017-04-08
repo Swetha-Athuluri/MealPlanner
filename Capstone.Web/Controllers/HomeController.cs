71d2200af4f7957323b2ae1fccf822cf97637768
Sweth ahuluri <swethapavana@gmail.com>
2017-04-08 14:56:27 -0400

    added the missing lines of code during the pull and push of committed codes

--- Capstone.Web/Controllers/HomeController.cs
+++ Capstone.Web/Controllers/HomeController.cs
@@ -1,19 +1,36 @@
-﻿using System;
+﻿using Capstone.Web.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Capstone.Web.Models.ViewModels;
+using Capstone.Web.Models;
 
 namespace Capstone.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private IRecipeDAL recipeDAL;
+        private IUserDAL userDAL;
+        private IRecipeIngredientDAL recipeIngredientDAL;
+        private IIngredientDAL ingredientDAL;
+        private IPreparationStepsDAL preparationStepsDAL;
+
+        public HomeController(IRecipeDAL recipeDAL, IUserDAL userDal, IRecipeIngredientDAL recipeIngredientDAL, IIngredientDAL ingredientDAL, IPreparationStepsDAL preparationStepsDAL)
+        {
+            this.recipeDAL = recipeDAL;
+            this.userDAL = userDal;
+            this.recipeIngredientDAL = recipeIngredientDAL;
+            this.ingredientDAL = ingredientDAL;
+            this.preparationStepsDAL = preparationStepsDAL;
+        }
 
         // GET: Home
         public ActionResult Index()
         {
 
-            return View("Index");
+            // return View("Index");
 
             List<RecipeViewModel> listOfRecipesView = new List<RecipeViewModel>();
             if (Session[SessionKeys.EmailAddress] != null)
@@ -42,9 +59,10 @@ namespace Capstone.Web.Controllers
                     listOfRecipesView.Add(rvm);
                 }
             }
-            else {
+            else
+            {
                 List<Recipe> recipes = recipeDAL.GetTop10RecentlyAddedRecipes();
-                
+
                 foreach (Recipe recipe in recipes)
                 {
                     List<RecipeIngredient> recipeIngredients = recipeIngredientDAL.GetRecipeIngredients(recipe.RecipeId);
@@ -68,7 +86,7 @@ namespace Capstone.Web.Controllers
                 }
             }
 
-            return View("Index",listOfRecipesView);
+            return View("Index", listOfRecipesView);
 
         }
 
